a6995dd1a1c9b03b284bfba4abc5772c2bb9d360
suriya <127676318+theSuriya@users.noreply.github.com>
2024-01-20 17:25:15 +0530

    Update server.py

--- server.py
+++ server.py
@@ -295,4 +295,4 @@ async def predict_mammals(file: UploadFile = File(...)):
 
 # Run The Server In Localhost via Uvicorn
 if __name__ == '__main__':
-     uvicorn.run(app,host='0.0.0.0',port=10000)
+     uvicorn.run(app,host='0.0.0.0',port=8000,port=int(os.environ.get('PORT', 8000)))
